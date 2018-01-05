7a4340318b1d7954f6a4139d17478215723f0c17
Linda Nab <L.Nab@lumc.nl>
2018-01-05 18:43:40 +0100

    add documentation for package

---
+++ R/mecor.R
@@ -0,0 +1,8 @@
+#' mecor: a measurement error correction package
+#'
+#' mecor provides correction methods for measurement
+#' errors in continuous endpoints.
+#'
+#' @section Package options:
+#' (...)
+"_PACKAGE"
