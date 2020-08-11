f03488c4ce7d61981c740781f97fa82e6d2c6a72
Linda Nab <l.nab@lumc.nl>
2020-08-11 23:14:02 +0200

    change print statement

--- R/S3-MeasErrorRandom.R
+++ R/S3-MeasErrorRandom.R
@@ -2,6 +2,6 @@
 print.MeasErrorRandom <- function(x){
   cat("\nCall:\n", deparse(attributes(x)$call), "\n", sep = "")
   cat("\nThe error-prone variable", deparse((attr(x, 'input')$substitute)),
-      "is assumed measured with random measurement error of magnitude", x$error)
+      "is assumed measured with random measurement error with variance", x$error)
   invisible(x)
 }
