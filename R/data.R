ff7503e362f5c582e630f06a5aa029fe17e210dc
Linda Nab <l.nab@lumc.nl>
2021-05-31 10:56:04 +0200

    add real data examples and remove fake sim data

--- R/data.R
+++ R/data.R
@@ -42,7 +42,28 @@
 #'
 #' Linda Nab, Maarten van Smeden, Renee de Mutsert, Frits R Rosendaal, and Rolf HH Groenwold. Sampling strategies for internal validation samples for exposure measurement error correction: A study of visceral adipose tissue measures replaced by waist circumference measures. American Journal of Epidemiology (2021). <doi:10.1093/aje/kwab114>
 "vat"
-#'  Low-dose iron supplements haemoglobin data [internal outcome-validation study]
+#' Visceral adipose tissue external data [external covariate-validation study]
+#'
+#' Waist circumference, visceral adipose tissue, sex, age, and total body fat of 100 individuals
+#'
+#' This is a simulated data set accompanying the dataset "vat", that is inspired by the NEO data <doi:10.1007/s10654-013-9801-3>. A motivating example using the example data can be found here: <doi:10.1093/aje/kwab114>
+#'
+#' @format A data frame with 100 rows and 5 variables:
+#' \describe{
+#'   \item{wc}{Waist circumference (standardised, cm)}
+#'   \item{vat}{Visceral adipose tissue (standardised, cm^2)}
+#'   \item{sex}{Sex (0 = male, 1 = female)}
+#'   \item{age}{Age (years)}
+#'   \item{tbf}{Total body fat (standardised, \%)}
+#' }
+#' @examples
+#' data("vat_ext", package = "mecor")
+#' @references
+#' Renee de Mutsert, Martin den Heijer, Ton J Rabelink, Johannes WA Smit, Johannes A Romijn, Johan W Jukema, Albert de Roos, Christa M Cobbaert, Margreet Kloppenburg, Saskia le Cessie, Saskia Middeldorp, Frits R Rosendaal. The Netherlands epidemiology of obesity (NEO) study: Study design and data collection. European Journal of Epidemiology (2013). <doi:10.1007/s10654-013-9801-3>
+#'
+#' Linda Nab, Maarten van Smeden, Renee de Mutsert, Frits R Rosendaal, and Rolf HH Groenwold. Sampling strategies for internal validation samples for exposure measurement error correction: A study of visceral adipose tissue measures replaced by waist circumference measures. American Journal of Epidemiology (2021). <doi:10.1093/aje/kwab114>
+"vat_ext"
+#' Low-dose iron supplements haemoglobin data [internal outcome-validation study]
 #'
 #' Capillary haemoglobin and venous haemoglobin levels of 400 subjects of a trial investigating the efficacy of low-dose iron supplements during pregnancy.
 #' Venous haemoglobin levels were observed of approximately 25\% of the subjects included in the trial.
@@ -62,6 +83,24 @@
 #'
 #' Linda Nab, Rolf HH Groenwold, Paco MJ Welsing, and Maarten van Smeden. Measurement error in continuous endpoints in randomised trials: Problems and solutions. Statistics in Medicine (2019). <doi:10.1002/sim.8359>
 "haemoglobin"
+#' Haemoglobin external data [external outcome-validation study]
+#'
+#' Capillary haemoglobin and venous haemoglobin levels of 100 individuals.
+#'
+#' This is a simulated data set accompanying the dataset "haemoglobin", that is inspired by a trial investigating low-dose iron supplements <doi:10.1093/ajcn/78.1.145>. A motivating example using the example data can be found here: <doi:10.1002/sim.8359>
+#'
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{capillary}{Haemoglobin levels measured in capillary blood (g/L)}
+#'   \item{venous}{Haemoglobin levels measured in venous blood (g/L)}
+#' }
+#' @examples
+#' data("haemoglobin_ext", package = "mecor")
+#' @references
+#' Maria Makrides, Caroline A Crowther, Robert A Gibson, Rosalind S Gibson, and C Murray Skeaff. Efficacy and tolerability of low-dose iron supplements during pregnancy: a randomized controlled trial. The American Journal of Clinical Nutrition (2003). <doi:10.1093/ajcn/78.1.145>
+#'
+#' Linda Nab, Rolf HH Groenwold, Paco MJ Welsing, and Maarten van Smeden. Measurement error in continuous endpoints in randomised trials: Problems and solutions. Statistics in Medicine (2019). <doi:10.1002/sim.8359>
+"haemoglobin_ext"
 #' TONE sodium data [outcome-calibration study]
 #'
 #' Self-reported sodium intake and urinary sodium in the TONE study, a randomized controlled trial designed to
@@ -84,125 +123,11 @@
 #'
 #' Ruth H Keogh, Raymond J Carroll, Janet A Tooze, Sharon I Kirkpatrick, Laurence S Freedman. Statistical issues related to dietary intake as the response variable in intervention trials. Statistics in Medicine (2016). <doi:10.1002/sim.7011>
 "sodium"
-#' Internal Covariate-Validation Study
+#' @references
+#' Maria Makrides, Caroline A Crowther, Robert A Gibson, Rosalind S Gibson, and C Murray Skeaff. Efficacy and tolerability of low-dose iron supplements during pregnancy: a randomized controlled trial. The American Journal of Clinical Nutrition (2003). <doi:10.1093/ajcn/78.1.145>
 #'
-#' A simulated dataset containing 1000 observations of the outcome Y, the
-#' error-prone exposure X_star and the covariate Z. The gold standard exposure
-#' X is observed in approximately 25\% of the study population
-#'
-#' @format A data frame with 1000 rows and 4 variables:
-#' \describe{
-#'   \item{Y}{outcome, continuous}
-#'   \item{X_star}{error-prone exposure, continuous}
-#'   \item{Z}{covariate, continuous}
-#'   \item{X}{gold standard exposure, continuous}
-#' }
-#' @examples
-#' data("icvs", package = "mecor")
-"icvs"
-#' Internal Outcome-Validation Study
-#'
-#' A simulated dataset containing 1000 observations of the error-prone outcome
-#' Y_star, the exposure X and the covariate Z. The gold standard outcome Y is
-#' observed in approximately 25\% of the study population
-#'
-#' @format A data frame with 1000 rows and 4 variables:
-#' \describe{
-#'   \item{Y_star}{error-prone outcome, continuous}
-#'   \item{X}{exposure, continuous}
-#'   \item{Z}{covariate, continuous}
-#'   \item{Y}{gold standard outcome, continuous}
-#' }
-#' #' @examples
-#' data("iovs", package = "mecor")
-"iovs"
-#' Internal Outcome Validation Study
-#'
-#' A simulated dataset containing 1000 observations of the error-prone outcome
-#' Y_star with differential measurement error, and the binary exposure X. The
-#' gold standard  outcome Y is observed in approximately 25\% of the study
-#' population
-#'
-#' @format A data frame with 1000 rows and 3 variables:
-#' \describe{
-#'   \item{Y_star}{error-prone outcome, continuous}
-#'   \item{X}{exposure, binary}
-#'   \item{Y}{gold standard outcome, continuous}
-#' }
-#' @examples
-#' data("iovs_diff", package = "mecor")
-"iovs_diff"
-#' Replicates Study
-#'
-#' A simulated dataset containing 1000 observations of the outcome Y, three
-#' replicate measures of the error-prone exposure X1_star, X2_star and X3_star
-#' and two covariates Z1 and Z2.
-#'
-#' @format A data frame with 1000 rows and 6 variables:
-#' \describe{
-#'   \item{Y}{outcome, continuous}
-#'   \item{X_star_1}{first replicate of error-prone exposure, continuous}
-#'   \item{X_star_2}{second replicate of error-prone exposure, continuous}
-#'   \item{X_star_3}{third replicate of error-prone exposure, continuous}
-#'   \item{Z1}{covariate, continuous}
-#'   \item{Z2}{covariate, continuous}
-#' }
-#' @examples
-#' data("rs", package = "mecor")
-"rs"
-#' Covariate-Calibration Study
-#'
-#' A simulated dataset containing 1000 observations of the outcome Y, the
-#' error-prone exposure X_star with systematic measurement error, the covariate
-#' Z and two replicates measures X1_star and X2_star of the exposure with
-#' classical measurement error. The two replicates are observed in the first
-#' 500 study participants.
-#'
-#' @format A data frame with 1000 rows and 5 variables:
-#' \describe{
-#'   \item{Y}{outcome, continuous}
-#'   \item{X_star}{error-prone exposure with systematic measurement error,
-#'   continuous}
-#'   \item{Z}{covariate, continuous}
-#'   \item{X_star_1}{first replicate of error-prone exposure with classical
-#'   measurement error, continuous}
-#'   \item{X_star_2}{second replicate of error-prone exposure with classical
-#'   measurement error, continuous}
-#' }
-#' @examples
-#' data("ccs", package = "mecor")
-"ccs"
-#' External Covariate-Validation Study
-#'
-#' A simulated dataset containing 100 observations of the gold standard
-#' measurement of the exposure X, the error-prone exposure X_star and the
-#' covariate Z. The outcome Y is not observed in the study population.
-#' To be used in combination with the dataset \link[mecor]{icvs}.
-#'
-#' @format A data frame with 100 rows and 3 variables:
-#' \describe{
-#'   \item{X}{gold standard exposure, continuous}
-#'   \item{X_star}{error-prone exposure, continuous}
-#'   \item{Z}{covariate, continuous}
-#' }
-#' @examples
-#' data("ecvs", package = "mecor")
-"ecvs"
-#' External Outcome-Validation Study
-#'
-#' A simulated dataset containing 100 observations of the gold standard
-#' measurement of outcome Y and the error-prone outcome Y_star. The covariates X
-#' and Z are not observed in the study population. To be used in combination
-#' with the dataset \link[mecor]{iovs}.
-#'
-#' @format A data frame with 100 rows and 2 variables:
-#' \describe{
-#'   \item{Y}{gold standard outcome, continuous}
-#'   \item{Y_star}{error-prone outcome, continuous}
-#' }
-#' @examples
-#' data("eovs", package = "mecor")
-"eovs"
+#' Linda Nab, Rolf HH Groenwold, Paco MJ Welsing, and Maarten van Smeden. Measurement error in continuous endpoints in randomised trials: Problems and solutions. Statistics in Medicine (2019). <doi:10.1002/sim.8359>
+"haemoglobin"
 #' Simulated dataset for the \link[mecor]{ipwm} function
 #'
 #' A simulated dataset containing 5000 observations of the covariates L1-L10,
